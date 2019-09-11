ca65ec13f34ca1be438691fe28c39800c2e17ef1
Thiago Franco de Moraes <totonixsame@gmail.com>
2019-09-11 15:05:02 -0300

    Added the script

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # blender_gnu_parallel_render
+
 Use GNU Parallel to render blender movies distributed by a bunch of nodes
+
+All nodes must have blender installed and be reachable using SSH with SSH keys and be listed at ~/.parallel/sshloginfile file, like this example:
+
+    1\ user@192.168.0.101
+    1\ user@192.168.0.102
+    1\ user@192.168.0.103
+    1\ user@192.168.0.104
+
+Just call this way:
+
+    render_blender_parallel.sh blender_file_name.blender start_frame end_frame ext
+
+Where:
+
+- `blender_file_name` is the blender file to be rendered.
+- `start_frame` is the number of first frame to be rendered.
+- `end_frame` is the number of the last frame to be rendered.
+- `ext` the extension of the output frames (ex. png, jpeg, etc.).
+
+The result is a folder containing the output frames. The folder will have the same name of `blender_file_name` parent folder plug `-frames`.
---
+++ render_blender_parallel.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+BLENDER_FULL_PATH=$(realpath $1)
+START_FRAME=$2
+END_FRAME=$3
+EXT=$(echo $4 | tr '[:lower:]' '[:upper:]')
+EXT_LOWER=$(echo $EXT| tr '[:upper:]' '[:lower:]')
+
+PROJ_FOLDER=$(dirname $BLENDER_FULL_PATH)/
+BLENDER_FILENAME=$(basename $BLENDER_FULL_PATH)
+PARENT_FOLDER=$(dirname $PROJ_FOLDER)/
+PROJ_FOLDER_REL=${PROJ_FOLDER#$PARENT_FOLDER}
+
+NUMBER_NODES=$(grep -v "^#" ~/.parallel/sshloginfile | wc -l)
+FRAMES_FOLDER=${PROJ_FOLDER_REL::-1}-frames
+
+# BLENDER_EXECUTABLE="/home/invesalius/Downloads/blender-2.79-linux-glibc219-x86_64/blender"
+BLENDER_EXECUTABLE="blender"
+
+echo $PROJ_FOLDER
+echo $BLENDER_FILENAME
+echo $PARENT_FOLDER
+echo $PROJ_FOLDER_REL
+echo $EXT
+echo $EXT_LOWER
+echo $START_FRAME
+echo $END_FRAME
+echo $FRAMES_FOLDER
+echo $NUMBER_NODES
+
+cd $PARENT_FOLDER
+
+echo "Transfering files"
+seq 1 $NUMBER_NODES | parallel --slf ~/.parallel/sshloginfile --progress --workdir /tmp/blender_rendering --plus --tf $PROJ_FOLDER_REL echo "Copied to \`hostname\`"
+echo "Transfered!"
+
+echo "Rendering"
+seq $START_FRAME $END_FRAME | parallel --slf ~/.parallel/sshloginfile --progress --workdir /tmp/blender_rendering --plus --return $FRAMES_FOLDER/{1}.$EXT_LOWER $BLENDER_EXECUTABLE --background ${PROJ_FOLDER_REL::-1}/$BLENDER_FILENAME -o "$FRAMES_FOLDER/#.$EXT_LOWER" -F $EXT -f {1}
+echo "Rendered"
+
+# echo "Removing files"
+# seq 1 $NUMBER_NODES | parallel --slf ~/.parallel/sshloginfile --progress --workdir /tmp/blender_rendering --plus rm -rf $PROJ_FOLDER_REL
+# seq 1 $NUMBER_NODES | parallel --slf ~/.parallel/sshloginfile --progress --workdir /tmp/blender_rendering --plus rm -rf $FRAMES_FOLDER
+# echo "Removed"
